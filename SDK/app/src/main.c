27d94be51b2a8714b8b53db4636fd5cc3e4f7629
TaterLi <admin@taterli.com>
2021-11-18 18:52:00 +0800

    添加Coremark例子

--- SDK/app/src/main.c
+++ SDK/app/src/main.c
@@ -2,28 +2,16 @@
 #include <stdio.h>
 #include "wm_hal.h"
 
-void Error_Handler(void);
+extern core_main(void);
 
 int main(void)
 {
-	SystemClock_Config(CPU_CLK_160M);
-	printf("enter main\r\n");
+	SystemClock_Config(CPU_CLK_240M);
+	HAL_Init();
+	core_main();
 	
 	while (1)
 	{
-		printf(".");
 		HAL_Delay(1000);
 	}
-}
-
-void Error_Handler(void)
-{
-	while (1)
-	{
-	}
-}
-
-void assert_failed(uint8_t *file, uint32_t line)
-{
-	printf("Wrong parameters value: file %s on line %d\r\n", file, line);
 }
